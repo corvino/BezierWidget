c1be7a9e20c8105e447914ad321037a80156bb2f
Nathan Corvino <nathan@corvino.com>
2015-04-02 14:55:19 -0500

    Restructure and simplify project.

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  TimingCurve
+//
+//  Created by Nathan Corvino on 3/31/15.
+//  Copyright (c) 2015 Nathan Corvino. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {}
+
